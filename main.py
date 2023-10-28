3a2a2ba4d3461c63855f77516809863a9930ff58
tharaka <tharakawiduranga22@gmail.com>
2023-10-29 00:09:07 +0530

    2nd commmit

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from tkinter import *
 from tkinter.ttk import *
 from time import strftime
 
-
 window=Tk()
 window.title("My Clock")
 
@@ -10,3 +9,10 @@ def time():
     string=strftime("%H:%M:%S  %p")
     label.config(text=string)
     label.after(1000,time)            # do again same function 1000 milliseconds(1 second)
+
+label=Label(window,font=("Arial Bold",50),background="black",foreground="yellow")
+label.grid(column=0,row=0)
+
+time()
+
+window.mainloop()
